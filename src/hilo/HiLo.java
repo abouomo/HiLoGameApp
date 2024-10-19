90edae8df914f332e1d05d04acb1bb835813d8d7
Abner Bouomo <abnerbouomo@gmail.com>
2024-10-18 21:17:53 -0400

    random number working

--- src/hilo/HiLo.java
+++ src/hilo/HiLo.java
@@ -1,5 +1,45 @@
 package hilo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class HiLo {
+
+	public static void main(String[] args) {
+
+		Random randomNumber = new Random();
+		Scanner scanner = new Scanner(System.in);
+
+		boolean playAgain = true;
+
+		while (playAgain) {
+			
+			int theRandomNumber = (randomNumber.nextInt(20) + 1);
+
+			int maxTries = 5;
+
+			System.out.println("Please enter a number between 1 and 20 ");
+			int userGuess = scanner.nextInt();
+
+			if (userGuess == theRandomNumber) {
+				System.out.println("You won!");
+			} else if ((userGuess > theRandomNumber)) {
+				System.out.println("Your guess is too high");
+			} else if ((userGuess < theRandomNumber)) {
+				System.out.println("Your guess is too low");
+			}
+			
+			System.out.println("Do you want to play again? yes or no");
+			String response = scanner.nextLine();
+			if (response.equalsIgnoreCase("yes")) {
+				return;
+			} else if (response.equalsIgnoreCase("no")) {
+				playAgain = false;
+			}
+		}
+		
+		scanner.close();
+
+	}
 
 }
